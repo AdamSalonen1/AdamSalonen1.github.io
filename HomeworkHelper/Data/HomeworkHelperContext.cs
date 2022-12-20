3ac0e5c9b875d7752cce48aa56ebb86b99ac0e03
AdamSalonen1 <102756507+AdamSalonen1@users.noreply.github.com>
2022-12-20 14:39:52 -0600

    Initial Commit

---
+++ HomeworkHelper/Data/HomeworkHelperContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HomeworkHelper.Models;
+
+namespace HomeworkHelper.Data
+{
+    public class HomeworkHelperContext : DbContext
+    {
+        public HomeworkHelperContext (DbContextOptions<HomeworkHelperContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HomeworkHelper.Models.Assignments> Assignments { get; set; } = default!;
+    }
+}
---
+++ HomeworkHelper/Models/Assignments.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeworkHelper.Models
+{
+    public class Assignments
+    {
+        public int ID { get; set; }
+        public string? ClassName { get; set; }
+        public string? AssignmentName { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime RemindMe { get; set; }
+    }
+}
